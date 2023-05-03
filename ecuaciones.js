0bae244c870245c3a8fcc7028a38079254ac447e
Sebatian2632 <vazquezreyes45@gmail.com>
2023-05-03 11:56:23 -0600

    Acomodo de los valores e impresión

--- ecuaciones.js
+++ ecuaciones.js
@@ -63,13 +63,57 @@ async function grafica_min()
     X_3I = ((v2 * x2_r3) - (x2_r2 * v3)) / ((x1_r2 * x2_r3) - (x2_r2 * x1_r3));
     Y_3I = ((x1_r2 * v3) - (v2 * x1_r3)) / ((x1_r2 * x2_r3) - (x2_r2 * x1_r3));
 
-    console.log('X_1I = ', X_1I);
-    console.log('Y_1I = ', Y_1I);
-    console.log('X_2I = ', X_2I);
-    console.log('Y_2I = ', Y_2I);
-    console.log('X_3I = ', X_3I);
-    console.log('Y_3I = ', Y_3I);
+    //Sustitución en las ecuaciones:
+    R1 = (x1_eo * X_1I) + (x2_eo * Y_1I);
+    R2 = (x1_eo * X_2I) + (x2_eo * Y_2I);
+    R3 = (x1_eo * X_3I) + (x2_eo * Y_3I);
 
+    //Agrelo para guardar los resultados:
+    let Resultados = [
+        ["R1", R1],
+        ["R2", R2],
+        ["R3", R3]];
+    
+    //For para imprimir los resultados en la consola
+        for(let i=0; i<Resultados.length;i++)
+    {
+        for(let j=0; j<Resultados[i].length; j++)
+        {
+            console.log(Resultados[i][j]);
+        }
+    }
 
+    console.log('Arreglo acomodado');
+    //Llamamos a la función para acomodar el arrelo
+    Acomodo(Resultados);
+
+    //For para imprimir el arreglo acomodado en la consola
+    for(let i=0; i<Resultados.length;i++)
+    {
+        for(let j=0; j<Resultados[i].length; j++)
+        {
+            console.log(Resultados[i][j]);
+        }
+    }
+    
+    //Imprime en pantalla el valor correcto, en teoria
+    console.log('El menor resultado que podemos obtener es '+ Resultados[1][1]);
 }
-
+//Función para el acomodo del arreglo
+function Acomodo(Resultados)
+{
+    let n = Resultados.length;
+    for(let i =0; i<n-1; i++)
+    {
+        for(let j=0; j<n-1;j++)
+        {
+            if(Resultados[j][1]> Resultados[j+1][1])
+            {
+                //Intercambio de valores
+                let temp = Resultados[j];
+                Resultados[j] = Resultados[j+1];
+                Resultados[j+1] = temp;
+            }
+        }
+    }
+}
